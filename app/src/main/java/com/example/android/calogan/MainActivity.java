465eab8f2dc80337ee0ec4b9a2a5363eae8e5894
Khwilo Kabaka <kwhyzzy@gmail.com>
2018-06-19 10:56:41 +0300

    Make the search button launch the Contact Display activity.

--- app/src/main/java/com/example/android/calogan/MainActivity.java
+++ app/src/main/java/com/example/android/calogan/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.android.calogan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mPhoneSearchBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPhoneSearchBtn = (Button) findViewById(R.id.phone_number_search_btn);
+
+        mPhoneSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ContactDisplay.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
