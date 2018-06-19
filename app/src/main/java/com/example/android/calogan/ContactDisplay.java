dd411fee8d7433250078c0661f2ee9fd9ba93782
Khwilo Kabaka <kwhyzzy@gmail.com>
2018-06-19 12:41:27 +0300

    Fetch phone number value from MainActivity and display it to the ContactsDisplayActivity.

--- app/src/main/java/com/example/android/calogan/ContactDisplay.java
+++ app/src/main/java/com/example/android/calogan/ContactDisplay.java
@@ -1,13 +1,23 @@
 package com.example.android.calogan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ContactDisplay extends AppCompatActivity {
+
+    private TextView mPhoneNumberTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_display);
+
+        mPhoneNumberTv = (TextView) findViewById(R.id.phone_number_value_tv);
+
+        Intent intent = getIntent();
+        String phoneNumberValue = intent.getExtras().getString("phone_number");
+        mPhoneNumberTv.setText(phoneNumberValue);
     }
 }
--- app/src/main/java/com/example/android/calogan/MainActivity.java
+++ app/src/main/java/com/example/android/calogan/MainActivity.java
@@ -5,25 +5,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText mPhoneSearchEt;
     private Button mPhoneSearchBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mPhoneSearchEt = (EditText) findViewById(R.id.phone_number_search_et);
         mPhoneSearchBtn = (Button) findViewById(R.id.phone_number_search_btn);
+
 
         mPhoneSearchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // Fetch the value of the EditText.
+                String phoneNumber = mPhoneSearchEt.getText().toString();
+
                 Intent intent = new Intent(MainActivity.this, ContactDisplay.class);
+
+                intent.putExtra("phone_number", phoneNumber);
 
                 startActivity(intent);
             }
         });
     }
+
+
 }
