62ad20d56d3281835b073a0eee94087873bc9715
Khwilo Kabaka <kwhyzzy@gmail.com>
2018-06-25 11:52:19 +0300

    Add the toString(), equals() and hashCode() methods.

--- app/src/main/java/com/example/android/calogan/Contact.java
+++ app/src/main/java/com/example/android/calogan/Contact.java
@@ -44,4 +44,34 @@ public class Contact {
     public void setCallDate(String callDate) {
         mCallDate = callDate;
     }
+
+    @Override
+    public String toString() {
+        return "Contact Name: " + getContactName()  + "\n" +
+                "Phone Number: " + getPhoneNumber() + "\n" +
+                "Call Type: " + getCallType() + "\n" +
+                "Call Date: " + getCallDate();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Contact) {
+            Contact temp = (Contact) obj;
+            if (this.mContactName.equals(temp.mContactName) &&
+                    this.mPhoneNumber.equals(temp.mPhoneNumber) &&
+                    this.mCallType.equals(temp.mCallType) &&
+                    this.mCallDate.equals(temp.mCallDate)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return (this.mContactName.hashCode() +
+                this.mPhoneNumber.hashCode() +
+                this.mCallType.hashCode() +
+                this.mCallDate.hashCode());
+    }
 }
